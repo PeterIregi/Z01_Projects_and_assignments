65d1aad95014a9597f80caa1a8b876070d24dd55
PeterIregi <iregipeter4@gmail.com>
2026-06-05 04:25:26 +0300

    Solve Camel to snake case

--- Checkpoint_Practice/piscine/level_3/cameltosnakecase.go
+++ Checkpoint_Practice/piscine/level_3/cameltosnakecase.go
@@ -3,28 +3,38 @@ package level_3
 func CamelToSnakeCase(s string )string{
 	//checkif  empty string
 	if s == ""{
-		return 
+		return ""
 	}
-	//check if only letters 
-	for _, ch := range s {
-		if !(ch >= 'a' && ch <= 'z') && (ch >= 'A' && ch <= 'Z'){
+	 
+	for i := 0; i< len(s);i++ {
+		c := s[i]
+		//check if only letters
+		if !(c >= 'a' && c <= 'z') &&  !(c >= 'A' && c <= 'Z'){
 			return s
 		}
-		
+		//check if it ends in caps
+		if i ==len(s)-1 && c >='A' && c <='Z'{
+			return s
+		}
+		//No consecutive uppercase letters
+		if i >0 && (c >='A' &&c<='Z') && (s[i-1]>='A' && s[i-1] <='Z'){
+			return s
+		} 
 	}
-	//check if it ends in caps
-	if s[len(s)-1] >='A' && s[len(s)-1]<='Z'{
-		return s
-	}
-	out := make([]byte, len(s))
+	
+	
+	
+
+
+	out := []byte{}
 
 	//convert to snake case
 	for  i:=0; i<len(s);i++{
 		c:= s[i]
-		if c >='A' && c<='Z'{
-			out = append(out,'-')
-			out = append(out,c)
+		if i>0 && c >='A' && c<='Z'{
+			out = append(out,'_')
 		}
 		out = append(out,c)
 	}
+	return string(out)
 }
